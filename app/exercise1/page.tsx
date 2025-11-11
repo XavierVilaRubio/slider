e9da796e10349c3b73dfeb4b77dcb06f6b5d67bb
Xavier Vila <xavier.vila@dribba.com>
2025-11-11 17:49:54 +0100

    feat(general): create base layout and Exercise1Page

---
+++ app/exercise1/page.tsx
@@ -0,0 +1,7 @@
+export default async function Exercise1Page() {
+  return (
+    <main className="w-full space-y-8 rounded-2xl border border-zinc-200 bg-white p-8 shadow-sm">
+      <h2 className="text-xl font-semibold text-gray-900">Selectable Range</h2>
+    </main>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="container mx-auto mt-16 flex justify-center gap-6">
+          {children}
+        </div>
       </body>
     </html>
   );
